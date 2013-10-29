39c3d760495a8593b8751e1080e1d676d8a21afe
Jackson Mudford <jacksonmudford@Jacksons-Mac-2.local>
2013-10-29 14:14:34 +1100

    Start of Project

---
+++ Learning Rope/Learning Rope/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Learning Rope
+//
+//  Created by Jackson Mudford on 29/10/13.
+//  Copyright (c) 2013 Jackson Mudford. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
