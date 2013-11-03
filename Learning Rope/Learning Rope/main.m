522af530ef8c4a1628f772008b8d419cdc627d7e
Jackson Mudford <jacksonmudford@Jacksons-Mac-2.local>
2013-11-03 11:47:38 +1100

    Update Program

--- Learning Rope/Learning Rope/main.m
+++ Learning Rope/Learning Rope/main.m
@@ -1,14 +1,32 @@
 #import <Foundation/Foundation.h>
-#import "Cars.h"
+
+@interface Person: NSObject{
+    int age;
+    int weight;
+}
+
+@property int age,weight;
+-(void) print;
+@end
+
+@implementation Person
+
+@synthesize age,weight;
+
+-(void) print{
+    NSLog(@"I am %i years old and weighs %i kilos", age, weight);
+}
+@end
 
 int main(int argc, char * argv[]){
-    
     @autoreleasepool{
-    Cars *cars = [[Cars alloc]init];
     
-        [cars fillingTank:100];
-        [cars cleanCar:100];
-        [cars print];
+    Person *Jackson = [[Person alloc]init];
+    
+    Jackson.age=20;
+    Jackson.weight=74;
+    
+    [Jackson print];
 }
     return 0;
 }
