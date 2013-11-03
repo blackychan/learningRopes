2cb1ef152c8cd962fd26d2ff1efd1d50af5cd984
Jackson Mudford <jacksonmudford@Jacksons-Mac-2.local>
2013-11-03 21:31:23 +1100

    Update Program

--- Learning Rope/Learning Rope/main.m
+++ Learning Rope/Learning Rope/main.m
@@ -1,5 +1,19 @@
 #import <Foundation/Foundation.h>
-#import "class.h"
+
+@interface Lesley:NSObject{
+    int a;
+}
+
+-(void)meth;
+
+@end
+
+@implementation Lesley
+-(void)meth{
+    a = 50;
+}
+@end
+
 
 int main(int argc, char * argv[]){
     @autoreleasepool{
